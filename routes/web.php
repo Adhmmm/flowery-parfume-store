b4311034e6a5a5874a0c60d01acaae5150f1b74d
Adhim <adhimm234@gmail.com>
2025-06-30 22:51:22 +0700

    install breeze untuk autentikasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\dashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,17 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('components.layout.app');
+    return view('welcome');
 });
-// Route::prefix('admin')->group(function () {
-//     Route::get('/dashboard', [dashboardController::class, 'index'])->name('admin.dashboard');
-// });
-Route::get('/admin/dashboard', function () {
-    return view('admin.index');
-})->name('admin.dashboard');
 
-Route::prefix('admin')->group(function () {
-    Route::view('/dashboard', 'admin.index')->name('admin.dashboard');
-    Route::view('/produk', 'admin.product')->name('admin.products');
-    Route::view('/kriteria', 'admin.kriteria')->name('admin.kriteria');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
