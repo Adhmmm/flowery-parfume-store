2f487f5510e8fd72d8729a1f25e9584f85760e15
Adhim <adhimm234@gmail.com>
2025-07-26 01:24:38 +0700

    merubah tampilan view produk index

--- resources/views/profile/admin/produk/index.blade.php
+++ resources/views/profile/admin/produk/index.blade.php
@@ -40,7 +40,7 @@
                 </div>
                 {{-- Button Tambah Data --}}
                 <div class="flex items-center justify-between gap-2 md:mt-0 mt-4">
-                    <a href="#"
+                    <a href="{{ route('admin.produk.create') }}"
                         class="px-3 py-2 flex items-center justify-between font-medium tracking-wide capitalize transition-colors duration-600 transform text-gray-50 bg-gradient-to-r from-violet-400 to-pink-300 border border-gray-200 rounded-md hover:from-emerald-400 hover:to-cyan-500 hover:text-slate-50 shadow-md">
                         + Add New Data</a>
                 </div>
@@ -122,63 +122,58 @@
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-slate-200">
-                                    <tr>
-                                        <td class="px-8 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                            <h2 class="text-sm font-semibold text-gray-500">1</h2>
-                                        </td>
-                                        <td class="px-auto py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                            <div class="inline-flex items-center gap-x-3">
+                                    @foreach ($produks as $produk)
+                                        <tr>
+                                            <td class="px-8 py-4 text-sm">{{ $loop->iteration }}</td>
+                                            <td class="px-auto py-4 text-sm">
                                                 <div class="flex items-center gap-x-2">
-                                                    <img class="object-cover w-10 h-10 rounded-md"
-                                                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                                                        alt="">
+                                                    <img class="w-10 h-10 rounded-md"
+                                                        src="{{ $produk->gambar ? asset('storage/' . $produk->gambar) : asset('storage/{$produk->gambar') }}"
+                                                        onerror="this.onerror=null;this.src='{{ asset('storage/{$produk->gambar') }}';" />
                                                     <div>
-                                                        <h2 class="font-medium text-neutral-800">Rose
-                                                            Garden
-                                                        </h2>
-                                                        <p class="text-sm font-normal text-gray-600">
-                                                            50ml</p>
+                                                        <h2 class="font-semibold">{{ $produk->nama }}</h2>
+                                                        <p class="text-sm text-gray-500">50ml</p>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                        <td class="px-8 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                            <h2 class="text-sm font-semibold text-gray-500">FP-RGN-001</h2>
-                                        </td>
-                                        <td class="px-7 py-4 text-sm font-semibold text-gray-500">
-                                            Parfume</td>
-                                        <td class="px-14 py-4 text-sm font-semibold text-gray-500">
-                                            75</td>
-                                        <td class="px-2 py-4 text-sm whitespace-nowrap">
-                                            <div class="flex items-center gap-x-2">
-                                                <p class="px-3 py-1 text-sm font-semibold text-gray-500">
-                                                    19.000</p>
-                                            </div>
-                                        </td>
-                                        <td class="px-auto py-4 text-sm whitespace-nowrap">
-                                            <div class="flex items-center gap-x-6">
-                                                <button
-                                                    class="text-gray-600 transition-colors duration-200 hover:text-red-500 focus:outline-none">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-5 h-5">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                    </svg>
-                                                </button>
-
-                                                <button
-                                                    class="text-gray-600 transition-colors duration-200 hover:text-yellow-500 focus:outline-none">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-5 h-5">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                    </svg>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                            </td>
+                                            <td class="px-8 py-4 text-sm">{{ $produk->sku }}</td>
+                                            <td class="px-2 py-4 text-sm capitalize">{{ $produk->kategori }}</td>
+                                            <td class="px-14 py-4 text-sm">{{ $produk->stok }}</td>
+                                            <td class="px-2 py-4 text-sm">Rp {{ number_format($produk->harga) }}</td>
+                                            <td class="px-auto py-4 text-sm whitespace-nowrap">
+                                                <div class="flex items-center gap-x-4">
+                                                    {{-- Tombol Hapus --}}
+                                                    <form action="{{ route('admin.produk.destroy', $produk->id) }}"
+                                                        method="POST"
+                                                        onsubmit="return confirm('Apakah Anda yakin ingin menghapus produk ini?');">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                            class="text-gray-600 mt-1 hover:text-red-500 transition-colors duration-200 focus:outline-none"
+                                                            title="Hapus Produk">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="w-5 h-5">
+                                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                            </svg>
+                                                        </button>
+                                                    </form>
+                                                    {{-- Tombol Edit --}}
+                                                    <a href="{{ route('admin.produk.edit', $produk->id) }}"
+                                                        class="text-gray-600 hover:text-yellow-500 transition-colors duration-200 focus:outline-none"
+                                                        title="Edit Produk">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5"
+                                                            stroke="currentColor" class="w-5 h-5">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                        </svg>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
