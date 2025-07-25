210f9288ffad0527e8bae9165ba5eb2ce27d58f9
Adhim <adhimm234@gmail.com>
2025-07-26 01:23:09 +0700

    merubah beberapa routing

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,16 @@ use App\Http\Controllers\Admin\ProdukController;
 use App\Http\Controllers\Admin\KriteriaController;
 use App\Http\Controllers\Admin\AlternatifController;
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\ResetPasswordController;
-use App\Http\Controllers\Auth\ProfileController as AuthProfileController;
+use App\Http\Controllers\User\LandingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,44 +31,93 @@ use App\Http\Controllers\Auth\ProfileController as AuthProfileController;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('landing.home');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// For Admin
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
-
-    // Dashboard
-    Route::get('/dashbard', [DashboardController::class, 'index'])->name('dashboard');
-
-    // Produk
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/produk', ProdukController::class)->names('produk');
-
-    // Kriteria
     Route::resource('/kriteria', KriteriaController::class)->names('kriteria');
-
-    // Alternatif
     Route::resource('/alternatif', AlternatifController::class)->names('alternatif');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->middleware('guest')->name('login');
+// User Landing
+Route::controller(LandingController::class)->group(function () {
+    Route::get('/', 'home')->name('landing.home');
+    Route::get('/produk', 'produk')->name('landing.produk');
+    Route::get('/about', 'about')->name('landing.about');
+    Route::get('/contact', 'contact')->name('landing.contact');
+});
 
-Route::get('/register', [RegisteredUserController::class, 'create'])
-    ->middleware('guest')
-    ->name('register');
+// Route::get('/login', function () {
+//     return view('auth.login');
+// })->middleware('guest')->name('login');
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register.store');
+// Route::get('/register', [RegisteredUserController::class, 'create'])
+//     ->middleware('guest')
+//     ->name('register');
 
+// Route::post('/register', [RegisteredUserController::class, 'store'])
+//     ->middleware('guest')
+//     ->name('register.store');
 
-Route::get('/register/success', function () {
-    return view('auth.register-success');
-})->name('register.success');
+// Route::get('/register/success', function () {
+//     return view('auth.register-success');
+// })->name('register.success');
+
+// Auth
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
+
+// Verify Email
+Route::middleware('auth')->group(function () {
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
+
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+});
 
 Route::post('/logout', function (\Illuminate\Http\Request $request) {
     \Illuminate\Support\Facades\Auth::logout();
@@ -84,6 +130,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(LandingController::class)->group(function () {
+    Route::get('/', 'home')->name('landing.home');
+    Route::get('/produk', 'produk')->name('landing.produk');
+    Route::get('/tentang', 'tentang')->name('landing.tentang');
 });
 
 require __DIR__ . '/auth.php';
