00854112b17e00f40f9767744dd9823d25743667
Adhim <adhimm234@gmail.com>
2025-07-26 01:26:44 +0700

    menambahkan layouting untuk landing page

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Flowery Parfum' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- Icons --}}
+    <script src="https://kit.fontawesome.com/6f03cac72d.js" crossorigin="anonymous"></script>
+
+    {{-- Alpine.js --}}
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=aclonica:400|bigshot-one:400|family=figtree:400,500,600&display=swap"
+        rel="stylesheet" />
+</head>
+
+<body class="bg-white text-gray-900">
+
+    {{-- Navbar --}}
+    @include('landing.partials.navbar')
+
+    <main class="min-h-screen pt-28">
+        {{ $slot }}
+    </main>
+
+    {{-- Footer --}}
+    @include('landing.partials.footer')
+
+</body>
+
+</html>
