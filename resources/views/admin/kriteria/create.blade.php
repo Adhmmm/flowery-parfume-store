c3dd03d6ea6ba50714f8bae96ba2e4800823bff4
Adhim <adhimm234@gmail.com>
2025-08-16 01:22:54 +0700

    mengubah routing

--- resources/views/admin/kriteria/create.blade.php
+++ resources/views/admin/kriteria/create.blade.php
@@ -51,7 +51,7 @@
         @endif
 
         <div class="bg-white p-6 mt-6 rounded-sm shadow-sm border border-gray-300 max-w-3xl">
-            <form action="{{ route('admin.kriteria.store') }}" method="POST" enctype="multipart/form-data"
+            <form action="{{ route('kriteria.store') }}" method="POST" enctype="multipart/form-data"
                 class="space-y-6">
                 @csrf
                 <div class="items-center justify-between md:mt-0 mt-4">
@@ -92,7 +92,7 @@
                 </div>
 
                 <div class="flex justify-end gap-3">
-                    <a href="{{ route('admin.kriteria.index') }}"
+                    <a href="{{ route('kriteria.index') }}"
                         class="inline-block px-6 py-2 border border-gray-400 rounded-xl text-sm font-medium text-gray-600 hover:bg-gray-100 transition">
                         Cancel
                     </a>
--- resources/views/admin/kriteria/index.blade.php
+++ resources/views/admin/kriteria/index.blade.php
@@ -1,5 +1,4 @@
 <x-admin-layout title="Kriteria SPK">
-    {{-- Header --}}
     <x-slot name="header">
         <nav class="bg-white w-full shadow-sm rounded-none">
             <div class="flex justify-between items-center h-20 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -41,7 +40,7 @@
                 </div>
 
                 <div class="flex items-center justify-between gap-2 md:mt-0 mt-4">
-                    <a href="{{ route('admin.kriteria.create') }}"
+                    <a href="{{ route('kriteria.create') }}"
                         class="px-3 py-2 flex items-center justify-between font-medium tracking-wide capitalize transition-colors duration-600 transform text-gray-50 bg-gradient-to-r from-violet-400 to-pink-300 border border-gray-200 rounded-md hover:from-emerald-400 hover:to-cyan-500 hover:text-slate-50 shadow-sm">
                         + Add New Data</a>
                 </div>
@@ -127,9 +126,8 @@
                                                 <span class="sr-only">rp</span>
                                             </td>
                                             <td class="px-12 py-4 text-sm whitespace-nowrap">
-                                                {{-- Tombol Hapus --}}
                                                 <div class="flex items-center gap-x-6">
-                                                    <form action="{{ route('admin.kriteria.destroy', $kriteria->id) }}"
+                                                    <form action="{{ route('kriteria.destroy', $kriteria->id) }}"
                                                         method="POST"
                                                         onsubmit="return confirm('Apakah Anda yakin ingin menghapus kriteria ini?');">
                                                         @csrf
@@ -145,8 +143,8 @@
                                                             </svg>
                                                         </button>
                                                     </form>
-                                                    {{-- Tombol Edit --}}
-                                                    <a href="{{ route('admin.kriteria.edit', $kriteria->id) }}"
+
+                                                    <a href="{{ route('kriteria.edit', $kriteria->id) }}"
                                                         class="text-gray-600 hover:text-yellow-500 transition-colors duration-200 focus:outline-none"
                                                         title="Edit Produk">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -158,29 +156,6 @@
                                                     </a>
                                                 </div>
                                             </td>
-                                            {{-- <td class="px-12 py-4 text-sm whitespace-nowrap">
-                                                <div class="flex items-center gap-x-6">
-                                                    <button
-                                                        class="text-gray-600 transition-colors duration-200 hover:text-red-500 focus:outline-none">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                            class="w-5 h-5">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                        </svg>
-                                                    </button>
-
-                                                    <button
-                                                        class="text-gray-600 transition-colors duration-200 hover:text-yellow-500 focus:outline-none">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                            class="w-5 h-5">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                            </td> --}}
                                         </tr>
                                     @endforeach
                                 </tbody>
